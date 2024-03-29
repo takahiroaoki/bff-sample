d2cdb42a6fa750e5d69f9dfbe755eb6a5ebc4e72
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2024-03-29 18:31:26 +0000

    rename performance.interceptor

--- app/src/common/interceptor/performance.interceptor.ts
+++ app/src/common/interceptor/performance.interceptor.ts
@@ -5,33 +5,24 @@ import { tap } from 'rxjs/operators';
 import { format } from 'util';
 
 @Injectable()
-export class PerformanceLoggingInterceptor implements NestInterceptor {
-    private static readonly START_LOG_FORMAT = '[START] host:%s user-agent:%s referer:%s';
-    private static readonly END_LOG_FORMAT = '[END] host:%s user-agent:%s referer:%s time:%sms';
+export class PerformanceInterceptor implements NestInterceptor {
+    private static readonly LOG_FORMAT = '[PerformanceInterceptor] time:%sms host:%s user-agent:%s referer:%s';
 
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
         const start = Date.now();
-
         const gqlContext = GqlExecutionContext.create(context)
         const req = gqlContext.getContext().req;
-        const startMessage = format(
-            PerformanceLoggingInterceptor.START_LOG_FORMAT,
-            req.headers['host'],
-            req.headers['user-agent'],
-            req.headers['referer'],
-        )
-        Logger.log(startMessage);
 
         return next
             .handle()
             .pipe(
                 tap(() => {
                     const endMessage = format(
-                        PerformanceLoggingInterceptor.END_LOG_FORMAT,
+                        PerformanceInterceptor.LOG_FORMAT,
+                        Date.now() - start,
                         req.headers['host'],
                         req.headers['user-agent'],
                         req.headers['referer'],
-                        Date.now() - start,
                     )
                     Logger.log(endMessage)
                 }),
