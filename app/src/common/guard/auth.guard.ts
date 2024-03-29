756cd1902f2c0f3f236b5f44a38f832e3c1db781
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2024-03-29 16:51:55 +0000

    update auth.guard.ts

--- app/src/common/guard/auth.guard.ts
+++ app/src/common/guard/auth.guard.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class AuthGuard {
-    async canActivate() {
-        // TODO: authenticate request
+export class AuthGuard implements CanActivate {
+    canActivate(
+        context: ExecutionContext,
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        const req = context.switchToHttp().getRequest<Request>();
+        if (req) {// TODO: implement authentication
+            return false;
+        }
         return true;
     }
 }
